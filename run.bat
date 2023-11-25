23e9d0984a16b08baa9052cffe17c0ca66ec4f27
刘子超 <liuzichao@192.168.1.4>
2023-11-25 20:25:47 +0800

    adjust scripts

--- run.bat
+++ run.bat
@@ -1,7 +1,14 @@
 cd data
+
 alex Lexer.x
 IF %ERRORLEVEL% NEQ 0 EXIT
 move Lexer.hs ../src
+
+happy Parser.y
+IF %ERRORLEVEL% NEQ 0 EXIT
+move Parser.hs ../src
+
 cd ..
+
 @REM stack run -- -d D:\liuzichao_ProjectX_Dev_D\ProjectX\Content\Script 
 stack run -- -f D:\liuzichao_ProjectX_Dev_D\ProjectX\Content\Script\Logic\BP_XHUD_C.lua
--- run.sh
+++ run.sh
@@ -1,10 +1,20 @@
 cd data
+
 alex Lexer.x
 if [[ $? -ne 0 ]]; then
   echo "alex Lexer.x error"
   exit 1
 fi
 mv Lexer.hs ../src
+
+happy Parser.y
+if [[ $? -ne 0 ]]; then
+  echo "happy Parser.y error"
+  exit 1
+fi
+mv Parser.hs ../src
+
 cd ..
+
 # stack run
 stack run -- -f ./res/Script/Logic/BP_XHUD_C.lua
